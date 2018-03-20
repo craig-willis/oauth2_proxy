8c2f1ce8071974465c9a2c775874ae7594a64e73
Craig Willis <willis8@illinois.edu>
2018-03-20 11:13:53 +0000

    Added support for additional scopes and tokens

--- providers/globus.go
+++ providers/globus.go
@@ -1,7 +1,9 @@
 package providers
 
 import (
-	"encoding/json"
+        "bytes"
+        "encoding/json"
+        "errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -11,6 +13,13 @@ import (
 
 type GlobusProvider struct {
 	*ProviderData
+}
+
+type Token struct {
+    AccessToken string  `json:"access_token"`
+    ExpiresIn   int     `json:"expires_in"`
+    Scope       string  `json:"scope"`
+    Tokens      []Token `json:"other_tokens"`
 }
 
 //https://auth.globus.org/.well-known/openid-configuration
@@ -45,7 +54,10 @@ func NewGlobusProvider(p *ProviderData) *GlobusProvider {
 		}
 	}
 	if p.Scope == "" {
-		p.Scope = "openid email profile"
+
+		// TODO:  scopes should be configurable
+		p.Scope = "openid email profile urn:globus:auth:scope:auth.globus.org:view_identities urn:globus:auth:scope:transfer.api.globus.org:all urn:globus:auth:scope:data.materialsdatafacility.org:all urn:globus:auth:scope:transfer.api.globus.org:all urn:globus:auth:scope:search.api.globus.org:search"
+
 	}
 	return &GlobusProvider{ProviderData: p}
 }
@@ -82,4 +94,77 @@ func (p *GlobusProvider) GetEmailAddress(s *SessionState) (string, error) {
 	}
 
 	return userinfo.Email, nil
+}
+
+func (p *GlobusProvider) Redeem(redirectURL, code string) (s *SessionState, err error) {
+	if code == "" {
+		err = errors.New("missing code")
+		return
+	}
+
+	params := url.Values{}
+	params.Add("redirect_uri", redirectURL)
+	params.Add("client_id", p.ClientID)
+	params.Add("client_secret", p.ClientSecret)
+	params.Add("code", code)
+	params.Add("grant_type", "authorization_code")
+	if p.ProtectedResource != nil && p.ProtectedResource.String() != "" {
+		params.Add("resource", p.ProtectedResource.String())
+	}
+
+	var req *http.Request
+	req, err = http.NewRequest("POST", p.RedeemURL.String(), bytes.NewBufferString(params.Encode()))
+	if err != nil {
+		return
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	var resp *http.Response
+	resp, err = http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	var body []byte
+	body, err = ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return
+	}
+
+	if resp.StatusCode != 200 {
+		err = fmt.Errorf("got %d from %q %s", resp.StatusCode, p.RedeemURL.String(), body)
+		return
+	}
+	globusTokens := Token{}
+
+	err = json.Unmarshal(body, &globusTokens)
+	if err == nil {
+
+
+		otherTokens := ""
+        	for _, token := range globusTokens.Tokens {
+			otherTokens += " " + token.Scope + "=" + token.AccessToken
+                }
+		fmt.Printf("Found access tokens: %s\n", otherTokens)
+
+
+                s = &SessionState{
+                        AccessToken: globusTokens.AccessToken,
+                        OtherTokens: otherTokens,
+                }
+		return
+        }
+
+	var v url.Values
+	v, err = url.ParseQuery(string(body))
+	if err != nil {
+		return
+	}
+	if a := v.Get("access_token"); a != "" {
+		fmt.Printf("using existing access token\n")
+		s = &SessionState{AccessToken: a}
+	} else {
+		err = fmt.Errorf("no access token found %s", body)
+	}
+	return
 }
